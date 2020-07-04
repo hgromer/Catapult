fdc578719f9af1002d2e9b10fa0f0abb07b12115
hgromer <nanug33@gmail.com>
2020-07-04 01:44:28 -0400

    add a way to gracefully shut down application

--- src/main/java/com/catapult/Application.java
+++ src/main/java/com/catapult/Application.java
@@ -1,6 +1,7 @@
 package com.catapult;
 
 import com.catapult.listener.KeyListener;
+import com.catapult.listener.QuitListener;
 import org.jnativehook.GlobalScreen;
 
 import java.util.logging.Level;
@@ -8,12 +9,14 @@ import java.util.logging.Logger;
 
 public class Application {
   public static void main(String[] args) throws Exception {
+    // Turn off JNativeHook logger because it's noisy
     Logger.getLogger(GlobalScreen.class.getPackage().getName())
         .setLevel(Level.OFF);
     if (!Platform.isMac()) {
-      throw new IllegalStateException("Platform must be mac");
+      throw new IllegalStateException("Mac is the only platform currently supported");
     }
     GlobalScreen.registerNativeHook();
+    GlobalScreen.addNativeKeyListener(new QuitListener());
     GlobalScreen.addNativeKeyListener(new KeyListener());
   }
 }
