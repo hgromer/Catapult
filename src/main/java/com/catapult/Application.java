ca168c857d87e43e886b1532c398c11af5ab4f9e
hgromer <nanug33@gmail.com>
2020-07-04 14:37:26 -0400

    add graceful shutdown on Application exception

--- src/main/java/com/catapult/Application.java
+++ src/main/java/com/catapult/Application.java
@@ -5,21 +5,33 @@ import com.catapult.listener.GlobalScreenManager;
 import com.catapult.listener.KeyListener;
 import com.catapult.listener.QuitListener;
 import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.slf4j.LoggerFactory;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Application {
-  public static void main(String[] args) throws Exception {
+  private static org.slf4j.Logger LOG = LoggerFactory.getLogger(Application.class);
+  public static void main(String[] args) {
     // Turn off JNativeHook logger because it's noisy
     Logger.getLogger(GlobalScreen.class.getPackage().getName())
         .setLevel(Level.OFF);
     if (!Platform.isMac()) {
       throw new IllegalStateException("Mac is the only platform currently supported");
     }
-    GlobalScreenManager.registerNativeHook();
-    GlobalScreenManager.addNativeKeyListener(new QuitListener());
-    GlobalScreenManager.addNativeKeyListener(new DisplayMonitorInfoListener());
-    GlobalScreenManager.addNativeKeyListener(new KeyListener());
+    try {
+      GlobalScreenManager.registerNativeHook();
+      GlobalScreenManager.addNativeKeyListener(new QuitListener());
+      GlobalScreenManager.addNativeKeyListener(new DisplayMonitorInfoListener());
+      GlobalScreenManager.addNativeKeyListener(new KeyListener());
+    } catch (Exception e) {
+      try {
+        LOG.error("Caught exception while trying to launch Catapult, attempting graceful shutdown...", e);
+        GlobalScreenManager.unregisterNativeHook();
+      } catch (NativeHookException ne) {
+        LOG.error("Unable to unregister native hooks", ne);
+      }
+    }
   }
 }
