23d37b4c138de6143b320dbdccdadc3ad12a5d28
Hernan Romer <nanug33@gmail.com>
2020-07-22 23:23:34 -0400

    consume activation events (#8)

--- src/main/java/com/catapult/listener/EventConsumerDispatcher.java
+++ src/main/java/com/catapult/listener/EventConsumerDispatcher.java
@@ -1,17 +1,58 @@
 package com.catapult.listener;
 
-import java.util.concurrent.*;
+import com.google.common.collect.ImmutableList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class EventConsumerDispatcher extends ThreadPoolExecutor {
-    public EventConsumerDispatcher() {
-        super(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(), new ThreadFactory() {
-            @Override
-            public Thread newThread(Runnable r) {
-                Thread thread = new Thread(r);
-                thread.setName("Catapult Event Consumer Dispatch Thread");
-                thread.setDaemon(true);
-                return thread;
-            }
-        });
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * This event dispatcher allows us to consume events
+ * https://github.com/kwhat/jnativehook/wiki/Usage#consuming-events-unsupported
+ */
+public class EventConsumerDispatcher extends AbstractExecutorService {
+  private static final Logger LOG = LoggerFactory.getLogger(EventConsumerDispatcher.class);
+
+  private final AtomicBoolean running = new AtomicBoolean(true);
+
+  @Override
+  public void execute(@Nonnull Runnable command) {
+    if (running.get()) {
+      // The command must be run synchronously or the event cannot
+      // be consumed/suppressed and will be propagated
+      command.run();
     }
+  }
+
+  @Override
+  public void shutdown() {
+    LOG.info("EventConsumerDispatcher shutting down");
+    running.set(false);
+  }
+
+  @Override
+  @Nonnull
+  public List<Runnable> shutdownNow() {
+    shutdown();
+    return ImmutableList.of();
+  }
+
+  @Override
+  public boolean isShutdown() {
+    return !running.get();
+  }
+
+  @Override
+  public boolean isTerminated() {
+    return !running.get();
+  }
+
+  @Override
+  public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+    return false;
+  }
 }
--- src/main/java/com/catapult/managers/OsManager.java
+++ src/main/java/com/catapult/managers/OsManager.java
@@ -3,8 +3,6 @@ package com.catapult.managers;
 import com.catapult.listener.GlobalScreenManager;
 import com.catapult.monitor.Monitor;
 import com.catapult.monitor.MonitorFactory;
-import com.sun.jna.platform.win32.User32;
-import com.sun.jna.platform.win32.WinDef;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
